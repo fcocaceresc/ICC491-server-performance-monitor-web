08e1957c7852e3bdad7427e2762e41c7d012673c
Francisco <fcocacerescolihuil@gmail.com>
2025-07-02 15:03:14 -0400

    add status endpoint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,17 @@
-from flask import Flask
+import datetime
+
+from flask import Flask, jsonify
 
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
-    def hello_world():
-        return 'Hello World!'
+    @app.route('/status')
+    def status():
+        return jsonify({
+            'timestamp': datetime.datetime.now(datetime.UTC).isoformat(),
+            'status': 200,
+            'message': 'OK'
+        }), 200
 
     return app
